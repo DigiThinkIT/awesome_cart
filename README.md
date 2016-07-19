d491c173fe70649ade884bbfd6db677f0a6b52c7
Ubuntu <ubuntu@ip-172-31-25-130.us-west-2.compute.internal>
2016-07-19 18:50:39 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Awesome Cart
+
+A one page checkout experience cart replacement for ERPNext
+
+#### License
+
+GPL-v3
---
+++ awesome_cart/config/docs.py
@@ -0,0 +1,11 @@
+"""
+Configuration for docs
+"""
+
+# source_link = "https://github.com/[org_name]/awesome_cart"
+# docs_base_url = "https://[org_name].github.io/awesome_cart"
+# headline = "App that does everything"
+# sub_heading = "Yes, you got that right the first time, everything"
+
+def get_context(context):
+	context.brand_html = "Awesome Cart"
