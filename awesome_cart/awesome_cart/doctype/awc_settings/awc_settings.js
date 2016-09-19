bb318c0f381e000e93a946d913059eb2372e63dd
Felipe Orellana <forellana@digithinkit.com>
2016-09-19 08:40:09 -0700

    Added missing generated doctype files

---
+++ awesome_cart/awesome_cart/doctype/awc_settings/awc_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, DigitThinkIt, Inc. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Awc Settings', {
+	refresh: function(frm) {
+
+	}
+});
