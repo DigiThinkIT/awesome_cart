643d96bc3df25a92cee9b6cf392dbec16c63ab14
Felipe Orellana <forellana@digithinkit.com>
2017-05-25 13:20:47 -0700

    Refactored shipping rate refreshing. Fixed totals as well

--- awesome_cart/awesome_cart/doctype/awc_transaction/awc_transaction.py
+++ awesome_cart/awesome_cart/doctype/awc_transaction/awc_transaction.py
@@ -66,13 +66,13 @@ class AWCTransaction(Document):
 					flags={"ignore_permissions": 1}
 				)
 
-			if self.get("shipping_method"):
-				quotation.append("taxes", {
-					"charge_type": "Actual",
-					"account_head": frappe.get_value("Awc Settings", "Awc Settings", "shipping_account"),
-					"description": self.get("shipping_method", "Shipping Charges"),
-					"tax_amount": self.get("shipping_fee", 0)
-				})
+			#if self.get("shipping_method"):
+			#	quotation.append("taxes", {
+			#		"charge_type": "Actual",
+			#		"account_head": frappe.get_value("Awc Settings", "Awc Settings", "shipping_account"),
+			#		"description": self.get("shipping_method", "Shipping Charges"),
+			#		"tax_amount": self.get("shipping_fee", 0)
+			#	})
 
 			# assign formatted address text
 			quotation.address_display = get_address_display(frappe.get_doc("Address", quotation.customer_address).as_dict())
