5f7967d3be9d72c0f3b927b0e45d9429ccf251cb
Felipe Orellana <forellana@digithinkit.com>
2017-05-30 15:53:04 -0700

    Moved 404 test prior to using data on product page

--- awesome_cart/templates/pages/awc_product.py
+++ awesome_cart/templates/pages/awc_product.py
@@ -15,13 +15,14 @@ def get_context(context):
 	print(route)
 
 	awc_item, item = get_awc_item_by_route(route)
-	context["awc_item"] = awc_item
-	context["item"] = item
-	context["cart_tag"] = cart_tag
-	context["title"] = item.get("name")
 
 	if not item or not awc_item:
 		frappe.local.flags.redirect_location = "404"
 		raise frappe.Redirect
+		
+	context["awc_item"] = awc_item
+	context["item"] = item
+	context["cart_tag"] = cart_tag
+	context["title"] = item.get("name")
 
 	return context
