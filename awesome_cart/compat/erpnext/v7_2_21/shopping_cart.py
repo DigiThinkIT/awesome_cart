92c31afbd10445bad3e0dcbe9ef587daeb712c33
Felipe Orellana <forellana@digithinkit.com>
2017-06-15 08:42:12 -0700

    Fixes tax calculations. Fixes zero rate items

--- awesome_cart/compat/erpnext/v7_2_21/shopping_cart.py
+++ awesome_cart/compat/erpnext/v7_2_21/shopping_cart.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe import throw, _
 
-from erpnext.shopping_cart.cart import get_cart_quotation, apply_cart_settings
+from erpnext.shopping_cart.cart import get_cart_quotation, apply_cart_settings, set_taxes
 from erpnext.shopping_cart.doctype.shopping_cart_settings.shopping_cart_settings \
 	import is_cart_enabled, get_shopping_cart_settings
 from erpnext.accounts.doctype.pricing_rule.pricing_rule import get_pricing_rule_for_item
