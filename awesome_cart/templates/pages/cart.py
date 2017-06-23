9f239191d3aaf62cda337e98953e750c953e8c6f
Sushant Nadkar <sushantnadkar@users.noreply.github.com>
2017-06-23 19:12:15 +0530

    changed address lable (#72)
    made state field optional
    added email field in address creation from click of next button of shipping form of awc

--- awesome_cart/templates/pages/cart.py
+++ awesome_cart/templates/pages/cart.py
@@ -28,7 +28,7 @@ def get_context(context):
 	default_country_doc = next((x for x in context["countries"] if x.name == default_country), None)
 
 	context["addresses"] = frappe.get_all("Address", filters={"email_id" : frappe.session.user}, fields="*")
-
+	
 	country_idx = context["countries"].index(default_country_doc)
 	context["countries"].pop(country_idx)
 	context["countries"] = [default_country_doc] + context["countries"]
