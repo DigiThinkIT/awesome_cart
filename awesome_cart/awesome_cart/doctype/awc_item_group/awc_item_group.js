22d7f2ba0583f45284c5334df6ed9206641bb55c
Felipe Orellana <forellana@digithinkit.com>
2018-01-25 17:14:55 +0000

    Adds support for coupon item groups to ease setup of multiple items cobinations

---
+++ awesome_cart/awesome_cart/doctype/awc_item_group/awc_item_group.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, DigitThinkIt, Inc. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('AWC Item Group', {
+	refresh: function(frm) {
+
+	}
+});
